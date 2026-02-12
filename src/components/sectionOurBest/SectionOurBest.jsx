5bcfaa3f3867760accccad327b0fb740a80ee140
Dissperss <subbotin.vladimir2018@ya.ru>
2026-02-12 12:27:32 +0300

    feat: variables for colors, fonts & links for best goods

--- src/components/sectionOurBest/SectionOurBest.jsx
+++ src/components/sectionOurBest/SectionOurBest.jsx
@@ -3,6 +3,7 @@ import styles from './SectionOurBest.module.css'
 import firstImg from '../../assets/homePage/best/first.png'
 import secondImg from '../../assets/homePage/best/second.png'
 import thirdImg from '../../assets/homePage/best/third.png'
+import { Link } from 'react-router-dom'
 
 export const SectionOurBest = () => {
     return (
@@ -11,23 +12,32 @@ export const SectionOurBest = () => {
             <div className={styles.best__wrapper}>
                 <div className={styles.best__item}>
                     <img src={firstImg} alt="Solimo Coffee" />
-                    <h3 className={styles.best__item_title}>
-                        Solimo Coffee Beans 2 kg
-                    </h3>
+
+                    <Link to="/about">
+                        <h3 className={styles.best__item_title}>
+                            Solimo Coffee Beans 2 kg
+                        </h3>
+                    </Link>
                     <span className={styles.best__item_price}>10.73$</span>
                 </div>
                 <div className={styles.best__item}>
                     <img src={secondImg} alt="Presto Coffee" />
-                    <h3 className={styles.best__item_title}>
-                        Presto Coffee Beans 1 kg
-                    </h3>
+
+                    <Link to="/about">
+                        <h3 className={styles.best__item_title}>
+                            Presto Coffee Beans 1 kg
+                        </h3>
+                    </Link>
                     <span className={styles.best__item_price}>15.99$</span>
                 </div>
                 <div className={styles.best__item}>
                     <img src={thirdImg} alt="AROMISTICO Coffee" />
-                    <h3 className={styles.best__item_title}>
-                        AROMISTICO Coffee 1 kg
-                    </h3>
+
+                    <Link to="/about">
+                        <h3 className={styles.best__item_title}>
+                            AROMISTICO Coffee 1 kg
+                        </h3>
+                    </Link>
                     <span className={styles.best__item_price}>6.99$</span>
                 </div>
             </div>
